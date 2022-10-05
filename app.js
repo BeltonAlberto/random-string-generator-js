decef879ba649194ecf1b8d8778445efc2b9bf89
BeltonAlberto <bymenete@gmail.com>
2022-10-05 21:53:23 +0200

    #updates

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let output = document.querySelector('.text');
 output.addEventListener('click', function randomingStrings() {
     const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYXAabcdefghijklmnopqrstuvwxyz1234567890';
-    const stringLength = 5;
+    const STRlENGTH = 5;
     let randomString = ' ';
-    for (let i = 0; i <= stringLength; i++) {
+    for (let i = 0; i <= STRLENGTH; i++) {
         randomString += CHARS[~~(Math.random() * CHARS.length)];
     }
     console.log(randomString);
-    console.log(CHARS.length);
     output.innerHTML = `${randomString}`;
 });
