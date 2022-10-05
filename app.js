388e94436f922d7eb392c15af016988643a89e21
BeltonAlberto <bymenete@gmail.com>
2022-10-05 17:56:15 +0200

    # Okay

---
+++ app.js
@@ -0,0 +1,13 @@
+let output = document.querySelector('#text');
+output.addEventListener('click', randomingStrings(8));
+
+// Can be usefull to create a unique IDs, Passwords, strings
+
+function randomingStrings(stringLength) {
+    const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYXAabcdefghijklmnopqrstuvwxyz1234567890';
+    let randomString = ' ';
+    for (let i = 0; i <= stringLength; i++) {
+        randomString += CHARS[~~(Math.random() * CHARS.length) + 1];
+    }
+    output.innerText = `${randomString}`;
+}
