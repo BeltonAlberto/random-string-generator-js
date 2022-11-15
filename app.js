1a52986e5da1e4e47f933f1da89681f5031e77f2
BeltonAlberto <bymenete@gmail.com>
2022-11-15 13:02:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const output = document.querySelector('.text');
 
 // String Generator function
 
-output.addEventListener('click', function randomingStrings() {
+/* output.addEventListener('click', function randomingStrings() {
     const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYXAabcdefghijklmnopqrstuvwxyz1234567890';
     let randomString = '';
     for (let i = 0; i <= 7; i++) {
         randomString += CHARS[~~(Math.random() * CHARS.length)];
     }
+    console.log(randomString);
+    this.innerHTML = `${randomString}`;
+});
+ */
+
+output.addEventListener('click', function randomingStrings() {
+    const randomString = Math.random().toString(36).slice(2);
     console.log(randomString);
     this.innerHTML = `${randomString}`;
 });
